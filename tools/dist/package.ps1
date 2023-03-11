3c0201deab6a035763fb1a1be7b268763a78a65f
pv <p@siberx.dev>
2023-03-11 17:38:59 +0300

    Update build tools

--- tools/dist/package.ps1
+++ tools/dist/package.ps1
@@ -8,4 +8,4 @@ $Version = & $($PSScriptRoot + "\steps\get-version.ps1")
 & $($PSScriptRoot + "\steps\compose-redscripts.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -Version ${Version}
 & $($PSScriptRoot + "\steps\create-zip-from-stage.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -DistDir ${DistDir} -Version ${Version}
 
-#Remove-Item -Recurse ${StageDir}
+Remove-Item -Recurse ${StageDir}
