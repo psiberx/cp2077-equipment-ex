30789b44182449cd93a4f5b4b8d9500155e7e563
pv <p@siberx.dev>
2023-03-15 18:38:39 +0300

    Update build tools

--- tools/dist/steps/compose-archives.ps1
+++ tools/dist/steps/compose-archives.ps1
@@ -4,3 +4,4 @@ $ArchiveDir = "${StageDir}/archive/pc/mod"
 
 New-Item -ItemType directory -Force -Path ${ArchiveDir} | Out-Null
 Copy-Item -Path "archive/packed/archive/pc/mod/*" -Destination ${ArchiveDir}
+Copy-Item -Path "archive/source/resources/*" -Destination ${ArchiveDir}
