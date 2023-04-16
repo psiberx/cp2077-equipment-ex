38dc76d9cb4afc87b4ef711bdee308f09c963100
pv <p@siberx.dev>
2023-04-17 00:52:11 +0300

    Add redscript hints

--- tools/dist/install-mod.ps1
+++ tools/dist/install-mod.ps1
@@ -5,6 +5,7 @@ $Version = & $($PSScriptRoot + "\steps\get-version.ps1")
 
 & $($PSScriptRoot + "\steps\compose-archives.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName}
 & $($PSScriptRoot + "\steps\compose-redscripts.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -Version ${Version}
+& $($PSScriptRoot + "\steps\compose-hints.ps1") -StageDir ${StageDir}
 & $($PSScriptRoot + "\steps\install-from-stage.ps1") -StageDir ${StageDir} -GameDir ${GameDir}
 
 Remove-Item -Recurse ${StageDir}
--- tools/dist/package-mod.ps1
+++ tools/dist/package-mod.ps1
@@ -6,6 +6,7 @@ $Version = & $($PSScriptRoot + "\steps\get-version.ps1")
 
 & $($PSScriptRoot + "\steps\compose-archives.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName}
 & $($PSScriptRoot + "\steps\compose-redscripts.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -Version ${Version}
+& $($PSScriptRoot + "\steps\compose-hints.ps1") -StageDir ${StageDir}
 & $($PSScriptRoot + "\steps\create-zip-from-stage.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -DistDir ${DistDir} -Version ${Version}
 
 Remove-Item -Recurse ${StageDir}
